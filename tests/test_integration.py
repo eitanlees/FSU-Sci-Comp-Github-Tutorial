6beb7c2913398aa7178aafa70f0e5e5dbf883d03
Eitan Lees <eitanlees@gmail.com>
2019-02-25 13:06:40 -0500

    add trapezoid test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,4 +1,9 @@
+import numpy as np
 from integration import *
 
-def test_trap():
-    return
+
+def test_trapezoid():
+    func = lambda x: np.sin(x)
+    est_I = trapezoid(func, 0, np.pi, 2000)
+    true_I = 2.0
+    assert np.isclose(est_I, true_I)
