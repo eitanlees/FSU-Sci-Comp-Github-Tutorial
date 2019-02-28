fb14e84499d6eb7831480a2eb787a3ee79829899
Eitan Lees <eitanlees@gmail.com>
2019-02-28 10:52:34 -0500

    Fix indentation

--- linalg.py
+++ linalg.py
@@ -29,21 +29,21 @@ def weighted_jacobi(A, x, b, omega=2/3, max_iter = 1000):
         OUTPUT:
             x     - estimation of the solution
         """
-    for k in range(max_iter):
-        x_new = np.zeros_like(x)
-        for i in range(A.shape[0]):
-            s1 = np.dot(A[i, :i], x[:i])
-            s2 = np.dot(A[i, i + 1:], x[i + 1:])
-            x_new[i] = (b[i] - s1 - s2) / A[i, i]
+        for k in range(max_iter):
+            x_new = np.zeros_like(x)
+            for i in range(A.shape[0]):
+                s1 = np.dot(A[i, :i], x[:i])
+                s2 = np.dot(A[i, i + 1:], x[i + 1:])
+                x_new[i] = (b[i] - s1 - s2) / A[i, i]
 
-        # Check to see if iteration has converged
-        if np.allclose(x, x_new, atol=1e-10, rtol=0.):
-            break
+            # Check to see if iteration has converged
+            if np.allclose(x, x_new, atol=1e-10, rtol=0.):
+                break
 
-        # Replace x with weight of omega
-        x = omega * x_new + (1 - omega) * x
+            # Replace x with weight of omega
+            x = omega * x_new + (1 - omega) * x
 
-    return x
+        return x
 
 def gauss_sidel(A, x, b):
     return x
