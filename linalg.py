250cb0d353a336dc109d11a5565bb7a58bc4edf8
Eitan Lees <eitanlees@gmail.com>
2019-02-28 10:15:23 -0500

    Add linear algebra file

---
+++ linalg.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+# Iterative Methods
+
+def jacobi(A, x, b):
+    return x
+
+def weighted_jacobi(A, x, b):
+    return x
+
+def gauss_sidel(A, x, b):
+    return x
+
+def sor(A, x, b):
+    return x
