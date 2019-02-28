2573fc802e0bde47a3bf4f080255ec2518a2fb95
Eitan Lees <eitanlees@gmail.com>
2019-02-28 14:23:19 -0500

    Reformat files to be PEP8 compliant

--- linalg.py
+++ linalg.py
@@ -12,11 +12,13 @@ import numpy as np
 
 # Iterative Methods
 
+
 def jacobi(A, x, b):
     return x
 
-def weighted_jacobi(A, x, b, omega=2/3, max_iter = 1000):
-        """Weighted Jacobi method for finding the approximate solution of Ax=b
+
+def weighted_jacobi(A, x, b, omega=2 / 3, max_iter=1000):
+    """Weighted Jacobi method for finding the approximate solution of Ax=b
 
         USAGE:
             sol = weighted_jacobi(A, x, b)
@@ -29,24 +31,26 @@ def weighted_jacobi(A, x, b, omega=2/3, max_iter = 1000):
         OUTPUT:
             x     - estimation of the solution
         """
-        for k in range(max_iter):
-            x_new = np.zeros_like(x)
-            for i in range(A.shape[0]):
-                s1 = np.dot(A[i, :i], x[:i])
-                s2 = np.dot(A[i, i + 1:], x[i + 1:])
-                x_new[i] = (b[i] - s1 - s2) / A[i, i]
+    for k in range(max_iter):
+        x_new = np.zeros_like(x)
+        for i in range(A.shape[0]):
+            s1 = np.dot(A[i, :i], x[:i])
+            s2 = np.dot(A[i, i + 1 :], x[i + 1 :])
+            x_new[i] = (b[i] - s1 - s2) / A[i, i]
 
-            # Check to see if iteration has converged
-            if np.allclose(x, x_new, atol=1e-10, rtol=0.):
-                break
+        # Check to see if iteration has converged
+        if np.allclose(x, x_new, atol=1e-10, rtol=0.0):
+            break
 
-            # Replace x with weight of omega
-            x = omega * x_new + (1 - omega) * x
+        # Replace x with weight of omega
+        x = omega * x_new + (1 - omega) * x
 
-        return x
+    return x
+
 
 def gauss_sidel(A, x, b):
     return x
+
 
 def sor(A, x, b):
     return x
--- tests/test_linalg.py
+++ tests/test_linalg.py
@@ -1,14 +1,19 @@
 import numpy as np
 from linalg import *
 
+
 def test_weighted_jacobi():
     # initialize the matrix
-    A = np.array([[10., -1., 2., 0.],
-                  [-1., 11., -1., 3.],
-                  [2., -1., 10., -1.],
-                  [0.0, 3., -1., 8.]])
+    A = np.array(
+        [
+            [10.0, -1.0, 2.0, 0.0],
+            [-1.0, 11.0, -1.0, 3.0],
+            [2.0, -1.0, 10.0, -1.0],
+            [0.0, 3.0, -1.0, 8.0],
+        ]
+    )
     # initialize the RHS vector
-    b = np.array([6., 25., -11., 15.])
+    b = np.array([6.0, 25.0, -11.0, 15.0])
 
     # initialize guess
     x = np.zeros_like(b)
